54fb680a16d8c7eae7813bef8726ce021adf96da
Jesse <101078250+JessePython@users.noreply.github.com>
2022-05-12 21:06:08 -0700

    Improved positioning of elements

--- src/componets/RenderPageBody.tsx
+++ src/componets/RenderPageBody.tsx
@@ -6,7 +6,7 @@ function RenderAllButtons(props ){
     let ReturnData = [];
     if(RENDER_PAGE_BODY_INFO[props.index].ShowBothButtons === true){
         ReturnData.push(
-        <div>
+        <div className="PageButtonsContainer">
             <button className='PageButton BtnBlack' href={RENDER_PAGE_BODY_INFO[props.index].LeftButtonLinkSource}>{RENDER_PAGE_BODY_INFO[props.index].LeftButtonText}</button>
             <button className='PageButton BtnWhite'href={RENDER_PAGE_BODY_INFO[props.index].RightButtonLinkSource}>{RENDER_PAGE_BODY_INFO[props.index].RightButtonText}</button>
         </div>
@@ -14,7 +14,7 @@ function RenderAllButtons(props ){
     }
     else{
         ReturnData.push(
-        <div>
+        <div className="PageButtonsContainer">
             <button className='PageButton BtnBlack' href={RENDER_PAGE_BODY_INFO[props.index].LeftButtonLinkSource}>{RENDER_PAGE_BODY_INFO[props.index].LeftButtonText}</button>
         </div>
         );      
@@ -23,15 +23,14 @@ function RenderAllButtons(props ){
 }
 
 function RenderPageBody(){
-    var hi = RENDER_PAGE_BODY_INFO[0].BodyTitle;
-    console.log(hi);
-    console.log(RENDER_PAGE_BODY_INFO.length);
     let ReturnedCode = [];
     for(let i = 0; i < RENDER_PAGE_BODY_INFO.length; i++){
         ReturnedCode.push(
             <div className='PageBody' style={{backgroundImage: "url(" + (RENDER_PAGE_BODY_INFO[i].BackgroundImage) + ")"}}>
-                <h2 className="PageHeader">{RENDER_PAGE_BODY_INFO[i].BodyTitle}</h2>
-                <p>{RENDER_PAGE_BODY_INFO[i].Description}<a href={RENDER_PAGE_BODY_INFO[i].DescriptionLinkSource}>{RENDER_PAGE_BODY_INFO[i].DescriptionLinkText}</a></p>
+                <div className = "PageBodyText">
+                    <h2 className="PageHeader">{RENDER_PAGE_BODY_INFO[i].BodyTitle}</h2>
+                    <p>{RENDER_PAGE_BODY_INFO[i].Description}<a href={RENDER_PAGE_BODY_INFO[i].DescriptionLinkSource}>{RENDER_PAGE_BODY_INFO[i].DescriptionLinkText}</a></p>
+                </div>
                 <RenderAllButtons index={i}/>
                 
             </div>

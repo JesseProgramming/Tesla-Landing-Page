3213b77f7345eb4c463baae7efd46be239e5320c
Jesse <101078250+JessePython@users.noreply.github.com>
2022-05-20 21:49:33 -0700

    Added bottom section scroll fading

--- src/componets/FadeAnimationTesting.js
+++ src/componets/FadeAnimationTesting.js
@@ -1,65 +1,33 @@
-
 $(document).ready(function(){
-    $("#div1").delay(500).animate({top: '+=50px', opacity:'show'},'slow', 'linear');
-    $("#div2").delay(500).animate({left: '-=50px', opacity:'show'},'slow', 'linear');
-    $("#div3").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
-    $("#div4").delay(100).animate({opacity:'show'},'slow', 'linear');
-    $("#d1").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
-});
+  /*
+  $("#Model3 > div > h2").hide();
+  $("#Model3 > div > h2").delay(1000).animate({top: '+=50px', opacity:'show'},'slow', 'linear');
+  $("#Model3 > div > p").hide();
+  $("#Model3 > div > p").delay(1500).animate({top: '+=50px', opacity:'show'},'slow', 'linear');
+  */
 
-//let isFooterHidden = false;
-$(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
-    let windowChangePadding = 150;
-    
-    if ( scrollTop > $("#s1").offset().top + $(window).height() - windowChangePadding && !isFooterHidden) { 
-      // display add
-      $("#d1").delay(100).animate({left: '-=50px', opacity:'hide'},'slow', 'linear');
-      isFooterHidden = true;
-    }
-    if ( scrollTop < $("#s1").offset().top + $(window).height() - windowChangePadding -5 && isFooterHidden) { 
-        // display add
-        $("#d1").delay(100).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
-        isFooterHidden = false;
-    }
 
-  });
-
+  //$("#div2").delay(500).animate({left: '-=50px', opacity:'show'},'slow', 'linear');
+  //$("#div3").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
+  $(".NavBarHeader").hide();
+  $(".NavBarHeader").delay(100).animate({opacity:'show'},'slow', 'linear');
+  //$( ".MainHeader" ).delay(300).scroll( "300px" );
   
+  //$(".BtnBlack").hide();
+  //$(".BtnWhite").hide();
 
+  /*
+  $(".BtnBlack").animate({left: '-=10%'}, 5);
+  $(".BtnWhite").animate({left: '+=10%'}, 5);
+  $(".BtnBlack").delay(500).animate({left: '+=50px', opacity:'80%'},'slow', 'linear');
+  $(".BtnWhite").delay(500).animate({left: '-=50px', opacity:'80%'},'slow', 'linear');
+  */
 
-
-  $(document).ready(function(){
-
-        /*
-        $("#Model3 > div > h2").hide();
-        $("#Model3 > div > h2").delay(1000).animate({top: '+=50px', opacity:'show'},'slow', 'linear');
-        $("#Model3 > div > p").hide();
-        $("#Model3 > div > p").delay(1500).animate({top: '+=50px', opacity:'show'},'slow', 'linear');
-        */
-
-
-        //$("#div2").delay(500).animate({left: '-=50px', opacity:'show'},'slow', 'linear');
-        //$("#div3").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
-        $(".NavBarHeader").hide();
-        $(".NavBarHeader").delay(100).animate({opacity:'show'},'slow', 'linear');
-        //$( ".MainHeader" ).delay(300).scroll( "300px" );
-        
-        //$(".BtnBlack").hide();
-        //$(".BtnWhite").hide();
-
-        /*
-        $(".BtnBlack").animate({left: '-=10%'}, 5);
-        $(".BtnWhite").animate({left: '+=10%'}, 5);
-        $(".BtnBlack").delay(500).animate({left: '+=50px', opacity:'80%'},'slow', 'linear');
-        $(".BtnWhite").delay(500).animate({left: '-=50px', opacity:'80%'},'slow', 'linear');
-        */
-
-        //$("#d1").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
-        $("#mainfooter").hide();
-        $("#ModelY > div").hide();
-        $("#ModelS > div").hide();
-    });
+  //$("#d1").delay(500).animate({left: '+=50px', opacity:'show'},'slow', 'linear');
+  $("#mainfooter").hide();
+  $("#ModelY > div").hide();
+  $("#ModelS > div").hide();
+});
 
 
 
@@ -68,7 +36,6 @@ $(window).scroll(function() {
   let isM3Hidden = false;
   let isMYHidden = true;
   let isMSHidden = true;
-  let scrollTestArray = [0,0];
 
 
   $(window).scroll(function(){
@@ -106,28 +73,10 @@ $(window).scroll(function() {
         isM3Hidden = false;
       }
 
-      
-      /*
-      //console.log(scrollTop < $(".MainHeader").offset().top + ($(window).height() * 2));
-      scrollTestArray[0] = $(".MainHeader").offset().top + ($(window).height() * 1) -100;
-      scrollTestArray[1] = $(".MainHeader").offset().top + ($(window).height() * 2) +100;
-      console.log(scrollTestArray[0] + ", " +scrollTestArray[1]);
-      //console.log($(window).scrollTop());
-      if ( scrollTop > $(".MainHeader").offset().top + ($(window).height() * 1) - 300
-        && scrollTop < $(".MainHeader").offset().top + ($(window).height() * 2) -300
-        && isMYHidden) { 
-        $("#ModelY > div").delay(2).animate({opacity:'show'}, 100, 'linear');
-        $("#ModelY > .PageButtonsContainer").delay(2).animate({opacity:'show'}, 100, 'linear');
-        $("#ModelY > a").delay(2).animate({opacity:'show'}, 100, 'linear');
-        isMYHidden = false;
-      }
-      else if(!isMYHidden){ 
-        $("#ModelY > div").delay(2).animate({opacity:'hide'}, 100, 'linear');
-        $("#ModelY > .PageButtonsContainer").delay(2).animate({opacity:'hide'}, 100, 'linear');
-        $("#ModelY > a").delay(2).animate({opacity:'hide'}, 100, 'linear');
-        isMYHidden = true;
-      }
-      */
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //Fade in & out when scrolling past the *top* of the page section
         var screenheighttop = $(".MainHeader").offset().top + ($(window).height() * 1);
         if($(window).scrollTop() - (screenheighttop - 300) > 0
         && $(window).scrollTop() - (screenheighttop - 400) < 400){
@@ -139,13 +88,12 @@ $(window).scroll(function() {
                 myOpacity = 1;
             $("#ModelY > div").css('display', 'block');
             $("#ModelY > div").css('opacity', myOpacity);
-            $("#ModelY > .PageButtonsContainer").css('opacity', myOpacity);
-            $("#ModelY > a").css('opacity', myOpacity);
         }
-        screenheighttop = $(".MainHeader").offset().top + ($(window).height() * 2);
-        if($(window).scrollTop() - (screenheighttop - 500) > 0
-        && $(window).scrollTop() - (screenheighttop - 600) < 400){
-            var myOpacity = ($(window).scrollTop() - (screenheighttop - 500)) / 300;
+        //Fade in & out when scrolling past the *bottom* of the page section
+        var screenbottom = $(".MainHeader").offset().top + ($(window).height() * 2);
+        if($(window).scrollTop() - (screenbottom - 200) > -200
+        && $(window).scrollTop() - (screenbottom - 300) < 0){
+            var myOpacity = Math.abs(($(window).scrollTop() - (screenbottom - 300))) / 200;
             console.log(myOpacity);
             if(myOpacity < 0.1)
                 myOpacity = 0;
@@ -153,11 +101,9 @@ $(window).scroll(function() {
                 myOpacity = 1;
             $("#ModelY > div").css('display', 'block');
             $("#ModelY > div").css('opacity', myOpacity);
-            $("#ModelY > .PageButtonsContainer").css('opacity', myOpacity);
-            $("#ModelY > a").css('opacity', myOpacity);
         }
 
-
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
       if (( scrollTop > $(".MainHeader").offset().top + $(window).height() * 3 - windowChangePadding && !isMSHidden)
       ||  ( scrollTop < $(".MainHeader").offset().top + $(window).height() * 2 - windowChangePadding + 200 && !isMSHidden)) { 
